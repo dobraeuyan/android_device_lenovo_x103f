a88ecba0e01828b9f6a0c3ec4d8da10352fa9ca3
dobraeuyan <fernandolopezp119@gmail.com>
2025-08-06 20:40:58 -0600

    Kill Qualcom Location, provide CABL

--- Android.mk
+++ Android.mk
@@ -1,9 +1,3 @@
-#
-# This empty Android.mk file exists to prevent the build system from
-# automatically including any other Android.mk files under this directory.
-#
-
-#include $(CLEAR_VARS)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),x103f)
@@ -11,17 +5,6 @@ ifeq ($(TARGET_DEVICE),x103f)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
-
-WIDEVINE_IMAGES := \
-    widevine.b00 widevine.b01 widevine.b02 widevine.b03 widevine.mdt
-
-WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(WIDEVINE_IMAGES))
-$(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Widevine firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(call vfatfilename,$(notdir $@)) $@
-
 
 WCNSS_CFG_INI := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 $(WCNSS_CFG_INI): $(LOCAL_INSTALLED_MODULE)
@@ -44,6 +27,6 @@ $(PERSIST_MAC): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /persist/$(notdir $@) $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS) $(WCNSS_CFG_INI) $(PERSIST_DICT) $(PERSIST_MAC) 
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_INI) $(PERSIST_DICT) $(PERSIST_MAC) 
 
 endif
